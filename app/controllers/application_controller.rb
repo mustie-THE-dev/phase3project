06d2440f2214e83d4fea29f8e2558fc1ef3d78aa
Simon kamau <simon.kamau@student.moringaschool.com>
2022-09-13 13:49:47 +0300

    reconfigured for sinatra

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,3 +8,49 @@ class ApplicationController < Sinatra::Base
       { message: "Good luck with your project!" }.to_json
     end
   
+    get "/pokemon" do
+        pokemon = Pokemon.all
+        pokemon.to_json(include: :reviews )
+      end
+    
+      get "/pokemon/:id" do
+        pokemon = Pokemon.find(params[:id])
+        pokemon.to_json(include: :reviews)
+      end
+    
+      get "/pokemon/:id/reviews" do
+        pokemon = Pokemon.find(params[:id])
+        pokemon.to_json(only: [], include: :reviews
+        )
+      end
+    
+      get "/reviews" do 
+        reviews = Review.all
+        reviews.to_json
+      end
+    
+      post "/reviews" do
+        review = Review.create(
+          rating: params[:rating],
+          text: params[:text],
+          pokemon_id: params[:pokemon_id]
+        )
+        review.to_json
+      end
+    
+      delete "/reviews/:id" do
+        review = Review.find(params[:id])
+        review.destroy
+        review.to_json
+      end
+    
+      patch "/reviews/:id" do
+        review = Review.find(params[:id])
+        review.update(
+          rating: params[:rating],
+          text: params[:text]
+        )
+        review.to_json
+      end
+    
+    end
